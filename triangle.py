f2c3f2db4577eced262b1389a4df2a08d3cf2787
jeshi2 <jamesvean19@gmail.com>
2024-08-14 20:25:30 +0300

    triangle

---
+++ triangle.py
@@ -0,0 +1,43 @@
+"""
+The new class will be called Triangle and this is what we want:
+
+    the constructor accepts three arguments – all of them are objects of the Point class;
+    the points are stored inside the object as a private list;
+    the class provides a parameterless method called perimeter(), which calculates the perimeter of the triangle described by the three points; the perimeter is the sum of all the lengths of the legs 
+"""
+
+import math
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x = x
+        self.__y = y
+
+    def getx(self):
+        return self.__x
+
+    def gety(self):
+        return self.__y
+
+    def distance_from_xy(self, x, y):
+        return math.hypot(abs(self.__x - x), abs(self.__y - y))
+
+    def distance_from_point(self, point):
+        return self.distance_from_xy(point.getx(), point.gety())
+
+
+class Triangle:
+    def __init__(self, vertice1, vertice2, vertice3):
+        self.__vertices = [vertice1, vertice2, vertice3]
+
+    def perimeter(self):
+        per = 0
+        for i in range(3):
+            per += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
+        return per
+
+
+triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+print(triangle.perimeter())
+    
